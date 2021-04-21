fa141acf39dc8311554ac3ee06086072507d7c69
Youmeng Hin <youmenghin@gmail.com>
2021-04-20 21:17:14 -0400

    Instantiated an array of length 5 with random values

--- raise_calculator.py
+++ raise_calculator.py
@@ -1,2 +1,5 @@
 #Import NumPy library
 import numpy as np
+#Instantiate an array of size 5 with random values of between 30,000 and 100,000
+a = np.random.randint(30000, 100000, (5), dtype=int)
+print(a)
