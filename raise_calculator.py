5480b0258e9be3cdae0c5cf6dd7e2cf124cc12a2
Youmeng Hin <youmenghin@gmail.com>
2021-04-20 21:53:15 -0400

    Imported CSV library

--- raise_calculator.py
+++ raise_calculator.py
@@ -1,5 +1,6 @@
 #Import NumPy library
 import numpy as np
+import csv
 #Instantiate an array of size 5 with random values of between 30,000 and 100,000
 a = np.random.randint(30000, 100000, (5), dtype=int)
 print(a)
