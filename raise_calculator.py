b0ca1465a89007eb1161704175a3eb5548f08f8c
Youmeng Hin <youmenghin@gmail.com>
2021-04-20 21:17:44 -0400

    Created a user input prompt

--- raise_calculator.py
+++ raise_calculator.py
@@ -3,3 +3,5 @@ import numpy as np
 #Instantiate an array of size 5 with random values of between 30,000 and 100,000
 a = np.random.randint(30000, 100000, (5), dtype=int)
 print(a)
+user = input("Enter the percentage of raise: ")
+print("Total payroll after " + user + "% raise... ")
