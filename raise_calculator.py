c9cf3683bf9aba08aa13d2922286c6365be087ab
Youmeng Hin <youmenghin@gmail.com>
2021-04-20 22:29:29 -0400

    Read employee_salries_1 with csv reader

--- raise_calculator.py
+++ raise_calculator.py
@@ -1,15 +1,19 @@
 #Import NumPy library
 import numpy as np
 import csv
-#Instantiate an array of size 5 with random values of between 30,000 and 100,000
+
+csv_file = open('employee_salaries_1.csv')
+csv_data = csv.reader(csv_file)
+
+# Instantiate an array of size 5 with random values of between 30,000 and 100,000
 a = np.random.randint(30000, 100000, (5), dtype=int)
 print(a)
 user = input("Enter the percentage of raise: ")
 print("Total payroll after " + user + "% raise... ")
-#Typecast user to int
+# Typecast user to int
 user = int(user)
 user = user / 100
-#Calculate raise
+# Calculate raise
 a = a + (a * user)
 user = str(user)
 print(a)
