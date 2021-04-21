45f46da5361b4919cb75ec3dc971e4d44041c547
Youmeng Hin <youmenghin@gmail.com>
2021-04-20 21:20:42 -0400

    Added calculation for post-raise salaries

--- raise_calculator.py
+++ raise_calculator.py
@@ -5,3 +5,10 @@ a = np.random.randint(30000, 100000, (5), dtype=int)
 print(a)
 user = input("Enter the percentage of raise: ")
 print("Total payroll after " + user + "% raise... ")
+#Typecast user to int
+user = int(user)
+user = user / 100
+#Calculate raise
+a = a + (a * user)
+user = str(user)
+print(a)
